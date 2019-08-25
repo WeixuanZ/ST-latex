9b530c9dc21d58af5bd6821174218694889b932b
Weixuan Zhang <39925558+WeixuanZ@users.noreply.github.com>
2019-08-25 13:06:26 +0800

    Bug fix and improvements
    Fix regex, can not insert `\frac{}{}` if no numerator

--- LaTeX/fraction.py
+++ LaTeX/fraction.py
@@ -12,19 +12,21 @@ class FractionCommand(sublime_plugin.TextCommand):
 		point = view.sel()[0].b
 		line = view.substr(sublime.Region(view.line(point).a, point))
 		line = line[::-1]
-		rex = re.compile(r"/(((\{\d+\}|\d)(\^|_))*(\d+)|([A-Za-z]+)(\\)?(\d*))")
-		expr = re.match(rex, line)
+		rex = re.compile(r"/(((\}\d+\{|\d)(\^|_))*([A-Za-z]+)(\\)?(\d*)|(\d+))")
+		rex2 = re.compile(r"/(\s*)")
+		expr = re.match(rex, line) # have numerator and /
+		expr2 = re.match(rex2, line) # no numerator, but have /
 		if expr:
 			numerator = expr.group(1)[::-1]
-			if numerator:
-				numerator_region = sublime.Region(point-len(numerator)-1,point)
-				view.erase(edit, numerator_region)
-				snippet = "\\\\frac{" + numerator + "}{$1}$0"
-			else:
-				snippet = "\\\\frac{$1}{$2}$0"
+			numerator_region = sublime.Region(point-len(numerator)-1,point)
+			view.erase(edit, numerator_region)
+			snippet = "\\\\frac{" + numerator + "}{$1}$0"
 			view.run_command("insert_snippet", {'contents' : snippet})
+			sublime.status_message("Fraction expanded")
+		elif expr2:
+			view.erase(edit, sublime.Region(point-1,point))
+			snippet = "\\\\frac{$1}{$2}$0"
+			view.run_command("insert_snippet", {'contents' : snippet})
+			sublime.status_message("WARNING: no numerator detected")
 		else:
 			sublime.status_message("ERROR: could not find fractions to expand")
-
-
-
