ce48155790d418fc53592afe92068057e5e3508e
Weixuan Zhang <39925558+WeixuanZ@users.noreply.github.com>
2019-08-30 21:51:41 +0800

    Update fraction.py
    No need to have `/` for expansion to work; allow for negative sign in front

--- LaTeX/fraction.py
+++ LaTeX/fraction.py
@@ -12,21 +12,34 @@ class FractionCommand(sublime_plugin.TextCommand):
 		point = view.sel()[0].b
 		line = view.substr(sublime.Region(view.line(point).a, point))
 		line = line[::-1]
-		rex = re.compile(r"/(((\}\d+\{|\d)(\^|_))*([A-Za-z]+)(\\)?(\d*)|(\d+))")
-		rex2 = re.compile(r"/(\s*)")
+		
+		rex = re.compile(r"(((\}\d+\{|\d)(\^|_))*([A-Za-z]+)(\\)?(\d*)|(\d+))(-?)")
+		rex2 = re.compile(r"(\s+)")
 		expr = re.match(rex, line) # have numerator and /
 		expr2 = re.match(rex2, line) # no numerator, but have /
+		
 		if expr:
-			numerator = expr.group(1)[::-1]
-			numerator_region = sublime.Region(point-len(numerator)-1,point)
-			view.erase(edit, numerator_region)
-			snippet = "\\\\frac{" + numerator + "}{$1}$0"
+			numerator = expr.group(1)[::-1] # without the negative sign
+			# negative sign in front
+			if expr.group(9):
+				numerator_region = sublime.Region(point-len(numerator)-1,point)
+				view.erase(edit, numerator_region)
+				snippet = "-\\\\frac{" + numerator + "}{$1}$0"
+			# no negative sign
+			else:
+				numerator_region = sublime.Region(point-len(numerator),point)
+				view.erase(edit, numerator_region)
+				snippet = "\\\\frac{" + numerator + "}{$1}$0"
 			view.run_command("insert_snippet", {'contents' : snippet})
 			sublime.status_message("Fraction expanded")
+		
 		elif expr2:
-			view.erase(edit, sublime.Region(point-1,point))
 			snippet = "\\\\frac{$1}{$2}$0"
 			view.run_command("insert_snippet", {'contents' : snippet})
 			sublime.status_message("WARNING: no numerator detected")
+		
 		else:
 			sublime.status_message("ERROR: could not find fractions to expand")
+
+
+
